a19d85ebefc20753fdc7a2a3a8bf8c286799a8df
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-04-18 09:58:38 -0300

    Início do projeto

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import axios from 'axios';
+import { BASE_URL, KEY } from "./requests";
+
+import App from '../src/App';
+
+const app = express();
+
+app.use('^/$', (req, res, next) => {
+    fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send("Some error happened");
+        }
+        return res.send(data.replace('<div id="root"></div>', `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`));
+    })
+})
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
+
+app.post('/filmes/:filme', function (req, res) {
+    const { filme } = req.params;
+    console.log(filme);
+    axios.get(`${BASE_URL}?apikey=${KEY}&t=${filme}`)
+        .then((response) => {
+            res.send(response.data);
+        });
+})
+
+app.listen(3030, () => {
+    console.log(`App launched on port 3030`);
+})
